586e79aa73586191b4e1d25b014a82215eb52ff2
Aarya <24khobaragadea@rbunagpur.in>
2026-08-11 19:45:38 +0530

    Rotate array left by one place

---
+++ Rotate_array_by_one.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class Rotate_array_by_one {
+    public static void main(String[] args) {
+        int[] arr={1,2,3,4,5,6,7};
+        int temp=arr[0];
+
+        for (int i=1;i<arr.length;i++){
+            arr[i-1]=arr[i];
+        }
+        arr[arr.length-1]=temp;
+        System.out.println("rotated array : "+Arrays.toString(arr));
+
+    }
+
+     
+}
